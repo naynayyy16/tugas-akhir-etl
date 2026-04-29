f385c4bce326884fa98943952798bf274366c214
naynayyy16 <222313365@stis.ac.id>
2026-04-29 22:36:00 +0700

    fresh start: remove heavy history

---
+++ bps_sawit.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:450f0987a26210aabb59bbb1fe7bd0f18e0677c2e4cd8e7bebc11701eefb1f06
+size 12921
---
+++ gee_ndvi_sumatera.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:de807add1aa0e4bfa0a29e33725293203244ea64e7193e1984a2a4e0a620b807
+size 125438
